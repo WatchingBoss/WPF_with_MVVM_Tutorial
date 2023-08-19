c936064aeee45cca51d4a8466f868026524a719a
ActionLS <opil94@yandex.ru>
2023-08-19 18:32:08 +0300

    Test of Model-Presentation
    ViewModel connection with View Windows works

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_with_MVVM_Tutorial.ViewModels.Base;
+
+namespace WPF_with_MVVM_Tutorial.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Window Title
+        private string _title = "Statistic Analyses CV";
+
+        /// <summary>Window Title</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+    }
+}
