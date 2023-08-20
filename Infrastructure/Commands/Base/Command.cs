2fc7d5b611a540cb950d874b3acfc1db774dfb8b
ActionLS <opil94@yandex.ru>
2023-08-20 05:52:41 +0300

    Cleaning

--- Infrastructure/Commands/Base/Command.cs
+++ Infrastructure/Commands/Base/Command.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace WPF_with_MVVM_Tutorial.Infrastructure.Commands.Base
