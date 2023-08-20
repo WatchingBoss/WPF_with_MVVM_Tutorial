7afb378b72cdab8c23ca58b3761cf73fb24e16ad
ActionLS <opil94@yandex.ru>
2023-08-20 05:19:20 +0300

    Add Status
    Add Status and bind it to TextBlock in StatusBar

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using WPF_with_MVVM_Tutorial.ViewModels.Base;
 
@@ -17,6 +19,17 @@ namespace WPF_with_MVVM_Tutorial.ViewModels
         {
             get => _title;
             set => Set(ref _title, value);
+        }
+        #endregion
+
+        #region Status : string - Status of program
+        private string _status = "Ready!";
+
+        /// <summary>Status of program</summary>
+        public string Status
+        {
+            get => _status;
+            set => Set(ref _status, value);
         }
         #endregion
     }
