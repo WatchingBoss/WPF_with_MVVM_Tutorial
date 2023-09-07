9f571e1a2f0b09cd2e4e7114b1520ade5324650c
ActionLS <opil94@yandex.ru>
2023-09-07 10:11:25 +0300

    Get remote data and change directory structure
    Request to remote server to get covid data csv file. Rename namespace and separate solution and project

--- CV_Console/Program.cs
+++ CV_Console/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
-using System.Net.WebSockets;
 using System.Reflection.Metadata.Ecma335;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CV_Console
@@ -22,7 +18,7 @@ namespace CV_Console
             return await result.Content.ReadAsStreamAsync();
         }
 
-        private static IEnumerable<string> GetDataLines()
+        private static IEnumerable<string> GetGataLines()
         {
             using var data_stream = GetDataStream().Result;
             using var data_reader = new StreamReader(data_stream);
@@ -35,40 +31,12 @@ namespace CV_Console
             }
         }
 
-        private static DateTime[] GetDates() => GetDataLines()
-            .First()
-            .Split(',')
-            .Skip(4)
-            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
-            .ToArray();
-
-        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
-        {
-            Regex regx = new (',' + "(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-            var lines = GetDataLines()
-                .Skip(1)
-                .Select(line => regx.Split(line));
-
-            foreach(var line in lines)
-            {
-                var province = line[0].Trim();
-                var country = line[1].Trim(' ', '"');
-                var counts = line.Skip(4).Select(int.Parse).ToArray();
-
-                yield return (country, province, counts);
-            }
-        }
-
         static void Main(string[] args)
         {
-            //foreach (var data_line in GetDataLines())
-            //    Console.WriteLine(data_line);
+            foreach (var data_line in GetGataLines())
+                Console.WriteLine(data_line);
 
-            //Console.WriteLine(string.Join("\r\n", GetDates()));
 
-            var russia_data = GetData()
-                .First(v => v.Country.Equals("Russia", StringComparison.OrdinalIgnoreCase));
-            Console.WriteLine(string.Join("\r\n", GetDates().Zip(russia_data.Counts, (date, count) => $"{date:dd:MM} - {count}")));
 
         }
     }
--- WPF_with_MVVM_Tutorial/ViewModels/MainWindowViewModel.cs
+++ WPF_with_MVVM_Tutorial/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
 using CV_WPF.Infrastructure.Commands;
-using CV_WPF.Models;
 using CV_WPF.ViewModels.Base;
 
 namespace CV_WPF.ViewModels
@@ -17,29 +14,7 @@ namespace CV_WPF.ViewModels
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecute, CanCloseApplicationCommandExecute);
 
             #endregion
-
-            var data_points = new List<DataPoint>((int)(360 / 0.1));
-            const double to_rad = Math.PI / 180;
-            for (double x = 0d; x < 360; x += 0.1)
-            {
-                double y = Math.Sin(x * to_rad);
-                data_points.Add(new DataPoint(x, y));
-            }
-            TestDataPoint = data_points;
         }
-
-        #region TestDataPoint : IEnumerable<DataPoint> - Test data for graph visualization
-        
-        private IEnumerable<DataPoint> _testDataPoint;
-
-        /// <summary>Test data for graph visualization</summary>
-        public IEnumerable<DataPoint> TestDataPoint
-        {
-            get => _testDataPoint;
-            set => Set(ref _testDataPoint, value);
-        }
-
-        #endregion
 
         #region Window Title
         private string title = "Statistic Analyses CV";
